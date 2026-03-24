f1d3874e9f592dfcf21e65eeaf83705c33c9c41a
el634dev <al7758066@gmail.com>
2026-03-24 11:48:33 -0400

    Build the main screen and task card

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.todo
 
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todo.ui.theme.TodoTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +47,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TodoScreen(modifier: Modifier = Modifier) {
+fun TodoScreen(modifier: Modifier = Modifier, todoViewModel: TodoViewModel = viewModel()) {
+    var taskBody by remember { mutableStateOf("") }
 
+    Column(
+        modifier = modifier.fillMaxSize()
+    ){
+        TextField(
+            modifier = Modifier.fillMaxWidth().padding(6.dp),
+            value = taskBody,
+            onValueChange = { taskBody = it },
+            label = { Text("Enter task") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    todoViewModel.addTask(taskBody)
+                    taskBody = ""
+                }
+            )
+        )
+    }
+}
+
+@Composable
+fun TaskCard(task: Task, toggleCompleted: (Task) -> Unit, modifier: Modifier = Modifier) {
+        Card(
+            modifier = modifier.padding(8.dp).fillMaxWidth()
+        ){
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Text(
+                    text = task.body,
+                    modifier = modifier.padding(start = 12.dp)
+                )
+                Checkbox(
+                    checked = task.completed,
+                    onCheckedChange = {
+                        toggleCompleted(task)
+                    }
+                )
+            }
+        }
 }
 
 @Preview(showBackground = true)
